0a6dc7452ad5ec7681eb843ec25462c3690fa7a2
cody-by-om <ompanda648@gmail.com>
2026-08-31 18:14:40 +0530

    Create index.js

---
+++ video-57/index.js
@@ -0,0 +1,51 @@
+// console.log("I am tutorail on loop")
+// let a = 1;
+// console.log(a);
+// console.log(a + 1);
+// console.log(a + 2);
+// console.log(a + 3);
+// console.log(a + 4);
+
+// Js Loop 
+
+
+
+//* For Loop
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i)
+// }
+
+//*
+
+
+//* For in Loop
+
+// let obj = {
+//     name: "Om",
+//     role: "Programer",
+//     company: "Code by om"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element);
+// }
+
+// for (const c of "om") {
+//     console.log(c)
+
+// }
+
+// let i = 0;
+// while (i < 6) {
+//     console.log(i)
+//     i++;
+// }
+
+let i = 0;
+do {
+    console.log(i)
+    i++;
+
+} while (i<6);
